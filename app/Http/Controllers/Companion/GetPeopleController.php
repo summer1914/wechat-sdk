db6e46c9391c7a96465556ce4cbf6b3dc54a6664
summer1914 <summer_jia@yeah.net>
2016-09-08 17:16:12 +0800

    验证通过

--- app/Http/Controllers/Companion/GetPeopleController.php
+++ app/Http/Controllers/Companion/GetPeopleController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use DB;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
 use App\Models\Wechat;
 
 class GetPeopleController extends Controller
@@ -15,13 +16,15 @@ class GetPeopleController extends Controller
         $signature = $request->get('signature');
         $timestamp = $request->get('timestamp');
         $nonce = $request->get('nonce');
-        $echoStr = $request->get('echoStr');
+        $echoStr = $_GET["echostr"];
 
         if (!Wechat::valid($nonce, $signature, $timestamp)) {
             abort(401);
         } else {
-            echo($echoStr);
-            return $echoStr;
+	   // Log::info('echoStr:'.$echoStr);
+          //  Log::info('request:'.'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']."\r\n");
+	  
+            return response()->json($echoStr);
         }
     }
 
