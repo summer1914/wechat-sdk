68f0bbb94c9a05547d149037a74b62e22d13d1c9
summer1914 <summer_jia@yeah.net>
2016-09-08 16:29:12 +0800

    添加认证日志

--- app/Models/Wechat.php
+++ app/Models/Wechat.php
@@ -73,7 +73,7 @@ class Wechat extends User
         if ($temStr === $signature) {
             return true;
         }
-        Log::info('valid failed: '.$_SERVER["REMOTE_ADDR"].(strstr($_SERVER["REMOTE_ADDR"],'101.226')? " FROM WeiXin": "Unknown IP".'sign:'.$signature.';codeSign:'.$temStr));
+        Log::info('valid failed: '.$_SERVER["REMOTE_ADDR"].(strstr($_SERVER["REMOTE_ADDR"],'101.226')? " FROM WeiXin": "Unknown IP".'sign:'.$signature.';codeSign:'.$temStr.';nonce:'.$nonce.';timestamp:'.$timestamp));
         return false;
     }
 
