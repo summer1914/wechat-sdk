deaff3759cd4b9e6d467c06c202033d8ceb326a9
summer1914 <summer_jia@yeah.net>
2016-09-09 16:37:26 +0800

    ...

--- app/Http/Controllers/Companion/GetPeopleController.php
+++ app/Http/Controllers/Companion/GetPeopleController.php
@@ -13,17 +13,20 @@ class GetPeopleController extends Controller
 {
     public function entarnce(Request $request)
     {
-        $signature = $request->get('signature');
-        $timestamp = $request->get('timestamp');
-        $nonce = $request->get('nonce');
-
-        $echoStr = $_GET["echostr"];
-
-
-        if (!Planet::valid($nonce, $signature, $timestamp)) {
-            abort(401);
-        } else {
-            return response()->json($echoStr);
+        $weObj = new Planet();
+        //$weObj->valid();//明文或兼容模式可以在接口验证通过后注释此句，但加密模式一定不能注释，否则会验证失败
+        $type = $weObj->getRev()->getRevType();
+        switch($type) {
+            case Wechat::MSGTYPE_TEXT:
+                    $weObj->text("hello, I'm wechat")->reply();
+                    exit;
+                    break;
+            case Wechat::MSGTYPE_EVENT:
+                    break;
+            case Wechat::MSGTYPE_IMAGE:
+                    break;
+            default:
+                    $weObj->text("help info")->reply();
         }
     }
 
