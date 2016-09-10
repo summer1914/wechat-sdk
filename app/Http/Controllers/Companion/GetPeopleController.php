2219204f6b923d30c23f74a83806070b9da8a42e
summer1914 <summer_jia@yeah.net>
2016-09-10 12:31:23 +0800

    更改路由

--- app/Http/Controllers/Companion/GetPeopleController.php
+++ app/Http/Controllers/Companion/GetPeopleController.php
@@ -14,7 +14,7 @@ class GetPeopleController extends Controller
     public function entarnce(Request $request)
     {
         Log::info("\n\nREMOTE_ADDR:".$_SERVER["REMOTE_ADDR"].(strstr($_SERVER["REMOTE_ADDR"],'101.226')? " FROM WeiXin": "Unknown IP"));
-        Log::info("(GET)QUERY_STRING:".$_SERVER["QUERY_STRING"]);
+        Log::info("(".$request->method().")QUERY_STRING:".$_SERVER["QUERY_STRING"]);
 
         $weObj = new Planet();
         $weObj->valid();//明文或兼容模式可以在接口验证通过后注释此句，但加密模式一定不能注释，否则会验证失败
@@ -37,33 +37,6 @@ class GetPeopleController extends Controller
                 $weObj->text("help info")->reply();
         }
 
-    }
-
-    public function message (Request $request)
-    {
-	    Log::info("\n\nREMOTE_ADDR:".$_SERVER["REMOTE_ADDR"].(strstr($_SERVER["REMOTE_ADDR"],'101.226')? " FROM WeiXin": "Unknown IP"));
-        Log::info("(POST)QUERY_STRING:".$_SERVER["QUERY_STRING"]);
-
-        $weObj = new Planet();
-        $weObj->valid();//明文或兼容模式可以在接口验证通过后注释此句，但加密模式一定不能注释，否则会验证失败
-        $type = $weObj->getRev()->getRevType();
-        switch($type) {
-            case Planet::MSGTYPE_TEXT:
-                $weObj->text("hello, I'm Planet")->reply();
-                exit;
-                break;
-            case Planet::EVENT_SUBSCRIBE:
-                $weObj->text($weObj->getRev()->getRevFrom())->reply();
-                break;
-            case Planet::EVENT_MENU_VIEW:
-                $weObj->text("hello, I'm redirect")->reply();
-                break;
-            case Planet::EVENT_SCAN:
-                $weObj->text($weObj->getRevSceneId ())->reply();
-                break;
-            default:
-                $weObj->text("help info")->reply();
-        }
     }
 
     public function makeCode()
